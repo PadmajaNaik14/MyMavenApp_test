b03c9565f009b72a762af6b87e79ba821f0153e8
PadmajaNaik14 <padmajagnaik@gmail.com>
2026-04-16 12:27:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Padmaja G Naik 1BI23CS145" );
+    }
+}
